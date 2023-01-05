a33cb67cfa79bcc25f7df0c5b49ce2ad9adc0dbe
renana malka <renana.malka@mail.huji.ac.il>
2023-01-05 13:27:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -66,7 +66,7 @@ int main(void)
 
   // Initialize the application. For example, create periodic timer(s) or
   // task(s) if the kernel is present.
-//  app_init();
+  // app_init();
 
 
   // Initialize callbacks
@@ -76,46 +76,24 @@ int main(void)
   counter_default_cfg( );
   delay_ms(300);
 
-  motors[left].speedControler.refSpeed = 50.0; // [Rad/sec]
 
-// Test functions for Debug
-//  test_gpio_init();
-//  test_pwm();
-//  read_hulls();
-//  test_motors_handle();
-//  runMotorNoHulls(left);
-//  init_test_callbacks_timed();
-//  test_moving_average();
-//  test_counter_spi();
-//  test_arduino_spi();
-//  test_counter_spi_once();
- // int iter =100;
+  // Test functions for Debug
+  //  test_gpio_init();
+  //  test_pwm();
+  //  read_hulls();
+  //  test_motors_handle();
+  //  runMotorNoHulls(left);
+  //  init_test_callbacks_timed();
+  //  test_moving_average();
+  //  test_counter_spi();
+  //  test_arduino_spi();
+  //  test_counter_spi_once();
+  // int iter =100;
 
-  //TODO: export real speeds to excel file
-  //outData.open("motorTest1");
-
-  float real_speeds[200]={-1} ;
-  float real_times[200]={0} ;
-  float speeds[]= {50,
-      40,
-      30,
-      40,
-      40,
-      -40,
-      -35,
-      -40,
-      -50,
-      -45,
-      -40,
-      -35,
-      -30,
-      30,
-      35,
-      0,
-      0
-
-
-} ;//[rad/sec],
+  float real_speeds[100] = {-1};
+//  float real_times[100] = {0};
+  float loc[100] = {0};
+  float speeds[4] = {30,30,30,30};//[rad/sec],
 
 
 #if defined(SL_CATALOG_KERNEL_PRESENT)
@@ -123,49 +101,33 @@ int main(void)
   sl_system_kernel_start();
 #else // SL_CATALOG_KERNEL_PRESENT
   uint64_t OuterStartTime = getuSec();
-
   uint64_t currentTime = getuSec();
   int i = 0;
-  int j = 0;
   int run_sec = 10;
-  //int run_usec = run_sec * 1000000;
+  while (currentTime - OuterStartTime < 10000000){
+      uint64_t InnerStartTime = getuSec();
+      // executeTimedFunctionsTest();
+      motors[left].speedControler.refSpeed =speeds[i%4];
 
-    while (currentTime - OuterStartTime < 10000000){
-        uint64_t InnerStartTime = getuSec();
-//      executeTimedFunctionsTest();
-        motors[left].speedControler.refSpeed =speeds[j%18];
-
-        while (getuSec() - InnerStartTime < 1000000){
-            // do nothing
-            //printf("boom1");
-        }
-        //printf("boom2");
-        //TODO: supposedly works, but make sure that an array of speeds if correctly fed into the motor (every one sec, speed increments)
-        //motors[left].speedControler.refSpeed =30.0;
-        executeTimedFunctions();
-        real_speeds[i]=  motors[left].speedControler.speedFromEncoder;
-        real_times[i]=  currentTime - OuterStartTime;
-        i++;
-        j++;
-        currentTime = getuSec();
-
-    // Do not remove this call: Silicon Labs components process action routine
-    // must be called from the super loop.
-//    sl_system_process_action();
-
-    // Application process.
-//    app_process_action();
-
-      #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
-          // Let the CPU go to sleep if the system allows it.
-          //sl_power_manager_sleep();
-      #endif
+      while (getuSec() - InnerStartTime < 1000000){
+          // do nothing
+      }
+      executeTimedFunctions();
+      real_speeds[i] = motors[left].speedControler.speedFromEncoder;
+//      real_times[i] = currentTime - OuterStartTime;
+      loc[i] = motors[left].speedControler.lastEncoderCnt;
+      i++;
+      currentTime = getuSec();
+      // Do not remove this call: Silicon Labs components process action
+      // routine must be called from the super loop:
+      // sl_system_process_action();
+      // Application process.
+      // app_process_action();
+    #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
+        // Let the CPU go to sleep if the system allows it.
+        //sl_power_manager_sleep();
+    #endif
   }
-  motors[left].speedControler.refSpeed = 0;
-  executeTimedFunctions();
-  while (1) {
-        getuSec() ;
-    }
 
 
     //return 0;
